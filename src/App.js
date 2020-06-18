8f376d121cfca59a167f19636ededda3af50fd3e
YamanKatmawi <yamankatmawi@gmx.de>
2020-06-18 17:28:40 +0200

    add more things

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./components/header/Header";
 import Data from "./data.json";
 import Post from "./components/post/Post";
+import "./scss/main.scss";
 
 function App() {
   return (
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,6 +4,7 @@ class Header extends React.Component {
   render() {
     return (
       <nav className="Nav">
+        <i className="fab fa-instagram"></i>
         <a className="Nav-logo" href="/">
           Instagram
         </a>
