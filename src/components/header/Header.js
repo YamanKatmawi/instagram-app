7da955b83ec78b7de8e87d3702fae64534963241
YamanKatmawi <yamankatmawi@gmx.de>
2020-06-20 18:08:32 +0200

    add more think

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,12 +3,25 @@ import "./header.css";
 class Header extends React.Component {
   render() {
     return (
-      <nav className="Nav">
-        <i className="fab fa-instagram"></i>
-        <a className="Nav-logo" href="/">
-          Instagram
-        </a>
-      </nav>
+      <div className="Header">
+        <div className="Instagram-logo">
+          <img src="./img/instagram.png" alt="Logo" />
+        </div>
+        <div className="search-logo">
+          <button>
+            {" "}
+            <i class="fas fa-search"></i>search
+          </button>
+        </div>
+        <ul className="header-icons">
+          <div>
+            <i class="fas fa-home"></i>
+            <i class="far fa-paper-plane"></i>
+            <i class="far fa-compass"></i>
+            <i class="far fa-heart"></i>
+          </div>
+        </ul>
+      </div>
     );
   }
 }
